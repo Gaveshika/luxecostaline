00da0ed165ce328e57016aa732f288f51a4bd52a
Uththara Jayawardana <gaveuththara1999@gmail.com>
2024-12-17 16:43:07 +0530

    created home page, package page, book page , and added pngs for the services, and added css for the pages.

---
+++ about.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatibale" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+    <!-- swiper css link -->
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<!--header section starts -->
+<section class="header">
+    <a href="home.php" class="logo">travel.</a>
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+        
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+
+
+<div class ="heading" style="background:url(images/heading-img-1.jpg) no-repeat">
+    <h1>about us</h1>
+</div>
+
+
+    <!-- swiper js link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <!-- custom js file link -->
+    <script src="js/script.js"></script>
+</body>
+
+<!--footer section starts -->
+<section class= "footer">
+    <div class="box-container">
+        
+        <div class="box">
+        <h3>quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i>home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i>about</a>
+        <a href="package.php"> <i class="fas fa-angle-right"></i>package</a>
+        <a href="book.php"> <i class="fas fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i>ask question</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>about us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>privacy policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i>terms of use</a>
+        </div>
+
+        
+        <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i> 091-25 44 190</a>
+        <a href="#"> <i class="fas fa-phone"></i> 091-20 40 290</a>
+        <a href="#"> <i class="fas fa-envelope"></i> luxecostalline@gmail.com</a>
+        <a href="#"> <i class="fas fa-map"></i> Wackwella Rd, Galle , Sri Lanka</a>    
+        </div>
+
+        <div class="box">
+        <h3>follow us</h3>
+        <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+        <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+        <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+        <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+          
+        </div>
+            
+    </div>
+    <div class="credit"> created by <span>Uththara Jayawardana</span> | all rights reserved! </div>
+
+
+
+ </section>
+<!--footer section ends -->
+</html>
